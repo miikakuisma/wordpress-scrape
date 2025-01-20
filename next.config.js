790bb965cdeff54d8a53d239fdcfd1e0b45771e3
Miika Kuisma <miika@sumo.app>
2025-01-20 15:19:11 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  }
+}
+
+module.exports = nextConfig 
